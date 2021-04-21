239313b1761f021263643a4f189c599fc76c7db6
Kuanysh Kuanysh <kuanysh@Kuanyshs-MacBook-Air.local>
2021-04-21 17:45:25 +0600

    init project

---
+++ android/app/src/main/java/com/startupjs_games_platform/MainActivity.java
@@ -0,0 +1,15 @@
+package com.startupjs_games_platform;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "startupjs_games_platform";
+  }
+}
